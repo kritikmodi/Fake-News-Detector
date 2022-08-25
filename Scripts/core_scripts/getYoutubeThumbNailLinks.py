b8ff085d05e346676d36882d24ddd1acbf495f66
ShreyaSriAN <75601363+ShreyaSriAN@users.noreply.github.com>
2022-08-26 00:28:10 +0530

    Add files via upload

---
+++ Scripts/core_scripts/getYoutubeThumbNailLinks.py
@@ -0,0 +1,24 @@
+api_key = 'AIzaSyC7NHT2t9sXCMBKhel1diaIBJ3GaBFt0dk'
+from googleapiclient.discovery import build
+yt = build('youtube','v3',developerKey=api_key)
+
+# keywords = ['government of india','parliament','rbi']
+def getThumbnail(keywords):
+    request = yt.search().list(part="snippet",
+        maxResults=25,
+        q=keywords)
+
+    response = request.execute()
+    image_urls =[]
+# print(response)
+    print("\n\n")
+    for i in range(25):
+        url = response['items'][i]["snippet"]['thumbnails']['high']['url']
+        image_urls.append(url)
+
+    for l in image_urls:
+        print(l)
+keywords = ['government of india']
+getThumbnail(keywords)
+
+
